0dc494b552e314d8f3848ce3f2819054a1bac267
talchow <talchow.chow6@gmail.com>
2025-01-05 21:06:07 +0800

    use tokio to implement the threadpool

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
-use web_server::ThreadPool;
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+// use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+// use web_server::ThreadPool;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-        pool.excute(|| {
-            handle_connection(stream);
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(stream).await;
         });
     }
 }
 
-fn handle_connection(mut stream:TcpStream) {
+async fn handle_connection(mut stream: TcpStream) {
+    use std::fs;
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = buf_reader.lines().next_line().await.unwrap().unwrap();
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
@@ -26,8 +32,7 @@ fn handle_connection(mut stream:TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
