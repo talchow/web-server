69f2657b7843ff2cd70c7e5d2e0620e4b488acc3
talchow <talchow.chow6@gmail.com>
2025-01-05 21:18:24 +0800

    use tokio::fs to read the file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
+use std::error::Error;
+
 // use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 // use web_server::ThreadPool;
 use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::{TcpListener, TcpStream},
+    fs, io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, net::{TcpListener, TcpStream}
 };
 
 #[tokio::main]
-async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+async fn main() -> Result< (),Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:7878").await?;
 
     loop {
-        let (stream, _) = listener.accept().await.unwrap();
+        let (stream, _) = listener.accept().await?;
         tokio::spawn(async move {
             handle_connection(stream).await;
         });
@@ -18,9 +19,7 @@ async fn main() {
 }
 
 async fn handle_connection(mut stream: TcpStream) {
-    use std::fs;
     let buf_reader = BufReader::new(&mut stream);
-    // let request_line = buf_reader.lines().next().unwrap().unwrap();
     let request_line = buf_reader.lines().next_line().await.unwrap().unwrap();
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
@@ -29,7 +28,8 @@ async fn handle_connection(mut stream: TcpStream) {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    // let contents = fs::read_to_string(filename).unwrap();
+    let contents = fs::read_to_string(filename).await.unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
